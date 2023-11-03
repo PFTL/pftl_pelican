33e0517a972bcc9d8791e6b121a027f88f43826e
Aquiles Carattino <aqui.carattino@gmail.com>
2023-11-03 18:55:48 +0100

    Import standard settings only. Add 404 page

--- settings_publish.py
+++ settings_publish.py
@@ -1,67 +1,14 @@
-# -*- coding: utf-8 -*-
-LOCALE = ('en_US',)
-DEFAULT_DATE_FORMAT = '%-d/%-m/%Y'
+import sys
+sys.path.append('.')
+from settings import *
 
-AUTHOR = u'Aquiles Carattino'
-SITENAME = u'Python For The Lab'
-SITEURL = u'https://pythonforthelab.com'
-TIMEZONE = 'Europe/Amsterdam'
-
-TEMPLATE_PAGES = {
-    'static_index.html': 'index.html',
-    'books.html': 'books/index.html',
-    'about.html': 'about/index.html',
-    }
-
-STATIC_PATHS = ['images', 'static']
 EXTRA_PATH_METADATA = {
     'static/robots.txt': {'path': 'robots.txt'},
     }
 
 STATIC_EXCLUDE_SOURCES = False
 
-ARTICLE_URL = 'blog/{slug}'
-ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
-
-PAGE_SAVE_AS = '{slug}/index.html'
-
-SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.9,
-        'indexes': 0.5,
-        'pages': 0.5
-        },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'weekly',
-        'pages': 'monthly'
-        },
-    'exclude': ['tag/', 'category/', 'categories.html', 'tags.html', 'search/'],
-    }
-
-FEED_DOMAIN = 'https://www.pythonforthelab.com'
-FEED_RSS = 'feed.rss'
-
-# MARKUP = ('rst', 'markdown',)
-
 RELATIVE_URLS = False
-
-INDEX_SAVE_AS = 'blog/index.html'
-
-DEFAULT_PAGINATION = 10
-
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.extra': {},
-        'markdown.extensions.meta': {},
-        'markdown.extensions.fenced_code': {},
-        'markdown.extensions.admonition': {},
-        },
-    'output_format': 'html5',
-    }
-
-SIMILAR_POSTS_MAX_COUNT = 3
 
 CSS_MIN = True
 HTML_MIN = True
